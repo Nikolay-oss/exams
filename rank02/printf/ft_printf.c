7fb0dced55759ef7fe5bfa8de567e660c0e08abe
Dipoo Kenchur <dkenchur@oa-i5.msk.21-school.ru>
2021-02-04 09:46:48 +0300

    update printf

--- rank02/printf/ft_printf.c
+++ rank02/printf/ft_printf.c
@@ -15,6 +15,14 @@ typedef	struct 		s_spec
 	int				bytes_count;
 }					t_spec;
 
+void	init_spec(t_spec *spec)
+{
+	spec->flag = FLG_NONE;
+	spec->width = -1;
+	spec->precision = -1;
+	spec->bytes_count = 0;
+}
+
 int		ft_strlen(char *str)
 {
 	int	count;
@@ -34,12 +42,34 @@ int		ft_putnstr(char *str, int c)
 	count = 0;
 	while (*(str + count) && *(str + count) != c)
 		count++;
+	write(1, str, count);
 	return (count);
+}
+
+int		ft_putlstr(char *str, int len)
+{
+	int i;
+
+	if (!str)
+		return (0);
+	write(1, str, len);
+	return (len);
 }
 
 int		ft_parser(char *format, t_spec *spec)
 {
+	int		bytes_count;
+	char	*current;
 
+	bytes_count = 0;
+	bytes_count = ft_putnstr(format, '%');
+	current = format + bytes_count;
+	while (*current)
+	{
+		current++;
+		init_spec(spec);
+		// to be continue...
+	}
 }
 
 int		ft_printf(const char *format, ...)
